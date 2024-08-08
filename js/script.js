d8ad4875bc0cd4906e4e6a7c47da1e9039b34e36
thiago-tsg <130875544+thiago-tsg@users.noreply.github.com>
2024-08-08 01:20:13 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const gameOver = document.querySelector('.game-over');
+const btn = document.querySelector('.btn');
+
+// Função para o Mario pular
+const jump = () => {
+    if (!mario.classList.contains('jump')) {
+        mario.classList.add('jump');
+
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+}
+
+// Função para reiniciar o jogo
+const restart = () => {
+    
+    mario.style.animation = '';
+    mario.style.bottom = '0';
+    mario.src = '../img/mario.gif';
+    mario.style.width = '150px';
+    mario.style.marginLeft = '0';
+
+    pipe.style.animation = 'pipe-animation 2s infinite linear';
+    pipe.style.left = '100%';
+
+    gameOver.style.display = 'none';
+    btn.style.display = 'none';
+
+    // Reinicia o loop de jogo
+    loop = setInterval(() => {
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +getComputedStyle(mario).bottom.replace('px', '');
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+
+            mario.src = '../img/game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '50px';
+
+            gameOver.style.display = 'flex';
+            btn.style.display = 'block';
+
+            clearInterval(loop);
+        }
+    }, 10);
+}
+
+// Inicializa o loop de jogo
+let loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '  ../img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        gameOver.style.display = 'flex';
+        btn.style.display = 'block';
+
+        clearInterval(loop);
+    }
+}, 10);
+
+// Adiciona o evento de pressionar qualquer tecla
+document.addEventListener('keydown', () => {
+    jump();
+    if (gameOver.style.display === 'flex') {
+        restart();
+    }
+});
