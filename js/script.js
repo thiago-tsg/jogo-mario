2026750441c5a89ea7968cf701d94a9366f8dc7c
thiago-tsg <130875544+thiago-tsg@users.noreply.github.com>
2024-08-08 01:25:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,22 +10,24 @@ const jump = () => {
 
         setTimeout(() => {
             mario.classList.remove('jump');
-        }, 500);
+        }, 500); // Duração da animação de pulo
     }
 }
 
 // Função para reiniciar o jogo
 const restart = () => {
-    
-    mario.style.animation = '';
+    // Reseta as propriedades do Mario
+    mario.style.animation = ''; // Remove qualquer animação aplicada
     mario.style.bottom = '0';
     mario.src = '../img/mario.gif';
     mario.style.width = '150px';
     mario.style.marginLeft = '0';
 
+    // Reseta a animação do tubo
     pipe.style.animation = 'pipe-animation 2s infinite linear';
     pipe.style.left = '100%';
 
+    // Esconde o game over e o botão
     gameOver.style.display = 'none';
     btn.style.display = 'none';
 
@@ -65,7 +67,7 @@ let loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = '  ../img/game-over.png';
+        mario.src = '../img/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
@@ -76,8 +78,15 @@ let loop = setInterval(() => {
     }
 }, 10);
 
-// Adiciona o evento de pressionar qualquer tecla
+// Adiciona o evento de pressionar qualquer tecla ou tocar na tela
 document.addEventListener('keydown', () => {
+    jump();
+    if (gameOver.style.display === 'flex') {
+        restart();
+    }
+});
+
+document.addEventListener('touchstart', () => {
     jump();
     if (gameOver.style.display === 'flex') {
         restart();
